4e2bf115ee22740edb91d447b3fb1babfff36e1b
hugumoraes <50657099+hugumoraes@users.noreply.github.com>
2019-12-15 06:17:46 -0300

    Loading 15-12

---
+++ ServerRPTest/Base-Shadow-Svnn/server-data/resources/[loading]/loadingscreen/__resource.lua
@@ -0,0 +1,22 @@
+files {	
+    'index.html',
+    'backgrounds/1.jpg',
+	'backgrounds/2.jpg',
+	'backgrounds/3.jpg',
+	'backgrounds/4.jpg',
+	'backgrounds/5.jpg',
+	'backgrounds/6.jpg',
+    'images/header-bg.png',
+	'images/logo.png',
+	'scripts/cycle.js',
+	'scripts/jquery.js',
+	'scripts/main.js',
+	'style.css',
+	'colour.css',
+	'music/audio.mp3',
+    'img/Logo.png'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
